943bdc899e873d50d321bfe102765b0de195fbc0
Gato 1 <webrepoleved@gmail.com>
2023-11-20 16:14:56 +0100

    Init vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+import Nav from './components/Nav.vue';
+</script>
+
+<template>
+  <div>
+  <header>
+
+      <Nav />
+
+  </header>
+
+  <RouterView />
+</div>
+</template>
+
---
+++ src/components/Nav.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
